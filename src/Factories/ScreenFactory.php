c02594f75e53388068d97f10be087af12e4e3fbb
Saul Chelewani <saul@patkay.net>
2020-03-25 13:36:02 +0200

    Added screen factory

--- src/Factories/ScreenFactory.php
+++ src/Factories/ScreenFactory.php
@@ -4,7 +4,25 @@
 namespace TNM\USSD\Factories;
 
 
-class ScreenFactory
-{
+use TNM\USSD\Http\Request;
+use TNM\USSD\Screen;
 
+abstract class ScreenFactory
+{
+    /**
+     * @var Request
+     */
+    private $request;
+    /**
+     * @var string
+     */
+    private $value;
+
+    public function __construct(Screen $screen)
+    {
+        $this->request = $screen->request;
+        $this->value = $screen->getRequestValue();
+    }
+
+    abstract public function make();
 }
