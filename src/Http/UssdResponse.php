b218be666c0cbb229826bd7b85a6b492de6ba32b
Saul Chelewani <saul@patkay.net>
2020-05-02 03:22:56 +0200

    Fixed request factory bug

--- src/Http/UssdResponse.php
+++ src/Http/UssdResponse.php
@@ -8,10 +8,9 @@ class UssdResponse implements UssdResponseInterface
 {
     public function respond($message, $type)
     {
-        $content = sprintf(
+        return sprintf(
             "<ussd><type>%s</type><msg>%s</msg><premium><cost>0</cost><ref>NULL</ref></premium></ussd>",
             $type, $message
         );
-        return response()->make($content);
     }
 }
