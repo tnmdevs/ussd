b470e3bf179d7255f0af357e36af1c7213ff5939
Saul Chelewani <saulchelewani@gmail.com>
2022-08-04 01:19:31 +0200

    setup testing

---
+++ tests/Feature/TruRouteAdapterTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace TNM\USSD\Test\Feature;
+
+use TNM\USSD\Test\TestCase;
+
+class TruRouteAdapterTest extends TestCase
+{
+
+    public function test_send_request_to_tru_route_adapter()
+    {
+        $this->artisan('make:ussd', ['name' => 'Welcome']);
+        $content = file_get_contents(__DIR__ . '/tru.route.request.xml');
+
+        $response = $this->call('POST', 'api/ussd', [], [], [], [], $content);
+
+        $response->assertOk();
+
+//        $response->assertOk();
+//        $response->dd();
+    }
+}
